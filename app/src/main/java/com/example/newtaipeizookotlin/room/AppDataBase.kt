db94c6edb167bbe62075faea23cd67d5ee9c7ff8
paul <paulgi@sinopac.com>
2022-04-22 15:47:13 +0800

    20220422 1.fix viewPager Room

--- app/src/main/java/com/example/newtaipeizookotlin/room/AppDataBase.kt
+++ app/src/main/java/com/example/newtaipeizookotlin/room/AppDataBase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 
-@Database(entities = [User::class], version = 2, exportSchema = false)
+@Database(entities = [User::class, ViewPagerListData::class], version = 3, exportSchema = false)
 abstract class AppDataBase : RoomDatabase() {
 
 
